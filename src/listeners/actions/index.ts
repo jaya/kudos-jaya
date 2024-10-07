d32588691576ceb9530d954b4c544dec5dc4f10b
Rodrigo Siqueira Cabral <rodrigo.cabral@luizalabs.com>
2024-10-07 14:25:09 -0300

    chore: remove unused code

--- src/listeners/actions/index.ts
+++ src/listeners/actions/index.ts
@@ -2,10 +2,8 @@ import { App } from '@slack/bolt';
 import chooseCardButtonCallback from './choose-card';
 import productPagesButtonCallback from './product-pages';
 import redeemButtonCallback from './redeem';
-import sampleActionCallback from './sample-action';
 
 const register = (app: App) => {
-  app.action('sample_action_id', sampleActionCallback);
   app.action('redeem_button', redeemButtonCallback);
   app.action('choose_card', chooseCardButtonCallback);
   app.action('products_page_next', productPagesButtonCallback);
--- src/listeners/commands/index.ts
+++ src/listeners/commands/index.ts
@@ -1,9 +1,7 @@
 import { App } from '@slack/bolt';
 import giveKudosCommandCallback from './give-kudos';
-import sampleCommandCallback from './sample-command';
 
 const register = (app: App) => {
-  app.command('/sample-command', sampleCommandCallback);
   app.command('/give-kudos', giveKudosCommandCallback);
 };
 
