b3f08c72d6f8f87d536f9f0deabb1d26326fab8c
Rodrigo Siqueira Cabral <rodrigo.cabral@luizalabs.com>
2025-01-31 10:12:20 -0300

    feat: add upload files test

--- src/utils/upload-files-slack.ts
+++ src/utils/upload-files-slack.ts
@@ -1,7 +1,6 @@
 import { WebAPICallResult } from '@slack/web-api';
 import * as fs from 'fs';
 import path from 'path';
-import logger from './logger';
 
 type SlackFile = {
   id: string;
@@ -43,25 +42,18 @@ type UploadFileResponse = {
 export async function uploadFile({ client, channelId }): Promise<string> {
   const dirPath = path.join(__dirname, '../assets');
   const filePath = path.join(dirPath, 'file.csv');
-  let channel_id: string;
-  try {
-    if (channelId.startsWith('U')) {
-      const conversationsResponse = await client.conversations.open({
-        users: channelId,
-      });
-      channel_id = conversationsResponse.channel.id;
-    }
 
-    const fileUploadResponse = await client.files.uploadV2({
-      channel_id,
-      file: fs.createReadStream(filePath),
-      filename: 'file.csv',
-    });
-    fs.unlinkSync(filePath);
-    const data = fileUploadResponse as UploadFileResponse;
-    return data?.files[0]?.files[0]?.url_private_download;
-  } catch (error) {
-    logger.error('uploadFile()', error);
-    throw error;
-  }
+  const conversationsResponse = await client.conversations.open({
+    users: channelId,
+  });
+  const channel_id = conversationsResponse.channel.id;
+
+  const fileUploadResponse = await client.files.uploadV2({
+    channel_id,
+    file: fs.createReadStream(filePath),
+    filename: 'file.csv',
+  });
+  fs.unlinkSync(filePath);
+  const data = fileUploadResponse as UploadFileResponse;
+  return data?.files[0]?.files[0]?.url_private_download;
 }
