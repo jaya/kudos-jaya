c6740b50ab82ba28981f2b5c65e5e2629961a8b7
Rodrigo Siqueira Cabral <rodrigo.cabral@luizalabs.com>
2025-01-31 16:16:39 -0300

    feat: add prizes-report-modal tests

---
+++ src/listeners/actions/test/prizes-report-modal.test.ts
@@ -0,0 +1,34 @@
+import logger from '@/utils/logger';
+import prizesReportModalCallback from '../prizes-report-modal';
+
+jest.mock('@/utils/logger');
+
+const ack = jest.fn();
+const client = {
+  views: {
+    open: jest.fn(),
+  },
+};
+const body = {
+  trigger_id: 'open_prizes_report_modal',
+};
+
+describe('prizesReportModalCallback()', () => {
+  it('Should build the prizes report modal successfully', async () => {
+    await prizesReportModalCallback({ ack, client, body });
+    expect(client.views.open.mock.calls[0][0].view).toMatchSnapshot();
+  });
+
+  it('Should log the error when fails to mount the component', async () => {
+    const error = new TypeError(
+      "Cannot read properties of undefined (reading 'trigger_id')"
+    );
+    await prizesReportModalCallback({ ack, client, body: undefined });
+    expect(logger.error).toHaveBeenCalledWith(
+      'prizesReportModalCallback() - Error trying to build structure',
+      {
+        error: error,
+      }
+    );
+  });
+});
