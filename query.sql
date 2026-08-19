e86a9249088965827049baa6bd7812895e7b1f1d
digicabral <digicabral@gmail.com>
2026-08-19 17:58:02 -0300

    chore: remove temporary scripts and config files

--- query.sql
+++
@@ -1,7 +0,0 @@
-SELECT u.id, u.name, u."teamId", 
-       i."teamName", i."defaultRecognitionChannel", 
-       i.bot
-FROM "user" u
-LEFT JOIN "installation" i ON u."teamId" = i."teamId"
-WHERE u.name = 'Mancha' OR i."teamName" = 'allset'
-LIMIT 10;
